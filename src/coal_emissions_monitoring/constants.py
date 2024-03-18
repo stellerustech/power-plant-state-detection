0ff57adda5e12f49618840569afb5ab48760d674
Mubin <mubin.md.hasan@gmail.com>
2024-03-19 03:35:07 +0800

    Fixed API URL, start and end date, COLLECTION

--- src/coal_emissions_monitoring/constants.py
+++ src/coal_emissions_monitoring/constants.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
 GLOBAL_EPSG = 4326
-API_URL = "https://earth-search.aws.element84.com/v0"
+API_URL = "https://earth-search.aws.element84.com/v1"
 COLLECTION = "sentinel-s2-l2a-cogs"  # Sentinel-2, Level 2A, COGs
 AOI_SIZE_METERS = 640
 IMAGE_SIZE_PX = 64
 CROP_SIZE_PX = 52
-START_DATE = datetime(year=2016, month=1, day=1)
-END_DATE = datetime(year=2019, month=12, day=31)
+START_DATE = datetime(year=2020, month=3, day=20)
+END_DATE = datetime(year=2023, month=3, day=30)
 MAX_DARK_FRAC = 0.5
 MAX_BRIGHT_MEAN = 250
 MAX_CLOUD_COVER_PRCT = 50
 TRAIN_VAL_RATIO = 0.8
-TEST_YEAR = 2020
+TEST_YEAR = 2021
 BATCH_SIZE = 32
 MAIN_COLUMNS = [
     "facility_id",
